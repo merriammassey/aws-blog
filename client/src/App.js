dffb3df06c60a710554b7d549bdc3ae8cb153cf7
Maria M <prov21_1@hotmail.com>
2022-04-25 13:32:06 -0700

    final changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import NoMatch from './pages/NoMatch';
-import Profile from './pages/Profile';
+import React from "react";
+import { Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <Router>
-    <div className="flex-column justify-flex-start min-100-vh">
-      <Header />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/profile/:username" component={Profile} />
-          <Route component={NoMatch} />
-        </Switch>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/profile/:username" element={<Profile />} />
+            <Route element={<NoMatch />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  </Router>
+    </Router>
   );
 }
 
